4a1f73ee5c903b5fa121157e69a972be1e8cbb8c
AnhAnNek <info.vananne@gmail.com>
2026-03-29 09:10:31 +0700

    fix: ProductDetailPage import errors

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -14,6 +14,7 @@ import { getReviewsByProductId } from "../services/reviewService";
 import type { Product } from "../types/product";
 import type { Review } from "../types/review";
 import { formatPrice } from "../utils/formatters";
+import { Text } from "../components/atoms/Text/Text";
 import "./ProductDetailPage.scss";
 
 const ProductDetailPage: React.FC = () => {
