e2b43cce0e603d891ae57dfea004faa337325f8d
antv-runs <antv@runsystem.net>
2026-04-07 15:20:36 +0700

    feat(cart): integrate redux cart into UI and user flow

--- src/components/molecules/HeaderActions/index.tsx
+++ src/components/molecules/HeaderActions/index.tsx
@@ -1,5 +1,7 @@
 import type { ComponentProps } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import type { RootState } from "@/store/cartStore";
 import { IconButton } from "@/components/atoms/IconButton";
 import "./index.scss";
 import { ROUTES } from "@/routes/paths";
@@ -12,18 +14,25 @@ export const HeaderMenuToggle: React.FC<HeaderMenuToggleProps> = (props) => {
 
 export const HeaderActions: React.FC = () => {
   const navigate = useNavigate();
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+  const totalQuantity = cartItems.reduce((acc, item) => acc + item.quantity, 0);
 
   return (
     <div className="header-icon">
-      <IconButton
-        className="header-icon__cart"
-        type="button"
-        ariaLabel="Cart"
-        svgName="icn_cart"
-        iconWidth={23}
-        iconHeight={21}
-        onClick={() => navigate(ROUTES.CART)}
-      />
+      <div className="header-icon__cart-wrapper">
+        <IconButton
+          className="header-icon__cart"
+          type="button"
+          ariaLabel="Cart"
+          svgName="icn_cart"
+          iconWidth={23}
+          iconHeight={21}
+          onClick={() => navigate(ROUTES.CART)}
+        />
+        {totalQuantity > 0 && (
+          <span className="header-icon__cart-badge">{totalQuantity}</span>
+        )}
+      </div>
       <IconButton
         className="header-icon__profile"
         type="button"
--- src/store/cartStore/index.ts
+++ src/store/cartStore/index.ts
@@ -2,14 +2,23 @@
 // The user has forbidden Redux Toolkit unless absolutely required, so using classic `legacy_createStore` API.
 import { legacy_createStore as createStore, combineReducers } from "redux";
 import { cartReducer } from "@/reducers/cartReducer";
+import { readStoredCartRows, writeStoredCartRows } from "@/utils/cartStorage";
 
 const rootReducer = combineReducers({
     cart: cartReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
 
-// Simple store setup, ready for middleware implementation (like Redux Thunk or Saga) later
-export const store = createStore(rootReducer);
+const preloadedState: any = {
+    cart: { items: readStoredCartRows() },
+};
+
+export const store = createStore(rootReducer, preloadedState);
+
+store.subscribe(() => {
+    writeStoredCartRows(store.getState().cart.items);
+});
 
 export default store;
